154f0f74836f422b9f4cb88337f9f9c156a48789
Csabi <csabykelemen@gmail.com>
2019-06-20 11:35:10 +0300

    Animal rescuer project

--- src/main/java/org/fasttrackit/Toy.java
+++ src/main/java/org/fasttrackit/Toy.java
@@ -1,16 +1,9 @@
 package org.fasttrackit;
 
-
-import java.time.Duration;
-
 public class Toy {
+
     private String toyColor;
     private String toyType;
-
-    public int toyStatus(int toyStatus) {
-        Duration.ofSeconds(20);
-        return 5;
-    }
 
     public String getToyColor() {
         return toyColor;
